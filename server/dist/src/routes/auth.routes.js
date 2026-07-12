98c52f402d05384be88112466e68d3abfd063ce7
Samindhya Jayasinghe <samindhyajayasinghe@gmail.com>
2026-07-13 01:50:54 +0530

    chore: exclude generated build files

--- server/dist/src/routes/auth.routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { login } from "../controllers/auth.controller.js";
-const authRouter = Router();
-authRouter.post("/login", login);
-export default authRouter;
